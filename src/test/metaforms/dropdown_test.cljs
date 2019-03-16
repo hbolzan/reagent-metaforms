6a9e647f097c4f72ec2e515db3d86d2366ee6015
Henrique Bolzan <henrique@bolzan.com.br>
2019-03-16 13:28:59 -0300

    Fix dropdown tests

--- src/test/metaforms/dropdown_test.cljs
+++ src/test/metaforms/dropdown_test.cljs
@@ -2,12 +2,21 @@
   (:require [cljs.test :refer (deftest is)]
             [metaforms.modules.complex-forms.components.dropdown :as dropdown]))
 
-(deftest review-dropdown-options-test
+(deftest dropdown-options-test
   (let [options [{:id "A"}
                  {:id "B"}
                  {:id "C"}]]
-    (is (= (dropdown/review-dropdown-options options "Other Field" :id :id-dscr)
-           [{:id nil :id-dscr "-- Other Field --"}
-            {:id "A" :id-dscr "A"}
-            {:id "B" :id-dscr "B"}
-            {:id "C" :id-dscr "C"}]))))
+    (is (= (dropdown/dropdown-options options "Other Field" :id :id)
+           [[:option {:value nil, :key 0} "-- Other Field --"]
+            [:option {:value "A", :key "A"} "A"]
+            [:option {:value "B", :key "B"} "B"]
+            [:option {:value "C", :key "C"} "C"]])))
+
+  (let [options [{:id 1 :name "First"}
+                 {:id 2 :name "Second"}
+                 {:id 3 :name "Third"}]]
+    (is (= (dropdown/dropdown-options options "Other Field" :id :name)
+           [[:option {:value nil, :key 0} "-- Other Field --"]
+            [:option {:value 1, :key 1} "First"]
+            [:option {:value 2, :key 2} "Second"]
+            [:option {:value 3, :key 3} "Third"]]))))
