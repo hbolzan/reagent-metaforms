2b68d88eb3096a12323d0f0e913d6cecc015b7ab
Henrique Bolzan <henrique@bolzan.com.br>
2019-05-31 23:57:07 -0300

    WIP - Bundle master form loading

--- src/metaforms/modules/complex_bundles/logic.cljs
+++ src/metaforms/modules/complex_bundles/logic.cljs
@@ -7,7 +7,7 @@
   (get-in db [:complex-bundles bundle-id]))
 
 (defn dissoc-definitions [bundled-tables]
-  (map #(dissoc (assoc % :definition-id (-> % :definition :id)) :definition) bundled-tables))
+  (map #(dissoc (assoc % :definition-id (-> % :definition first :id)) :definition) bundled-tables))
 
 (defn load-bundle-definition-success [db bundle-id bundle]
   (assoc-in db
@@ -17,9 +17,10 @@
                    (dissoc-definitions (:bundled-tables bundle)))))
 
 (defn bundled-form-id [bundle-id table-definition]
-  (keyword bundle-id (-> table-definition :definition :id)))
+  (keyword bundle-id (-> table-definition :id)))
 
 (defn bundle-forms-ids [bundle]
+  ;; (js/console.log bundle)
   (map #(keyword (:id bundle) (:definition-id %)) (:bundled-tables bundle)))
 
 (defn children-bundled-tables-ids [bundle-id bundled-tables]
@@ -29,7 +30,7 @@
   (let [first-definition (-> bundled-tables first :definition first)]
     (reduce
      (fn [new-db bundled-table] (load-definition-fn new-db
-                                                   (bundled-form-id bundle-id (:definition bundled-table))
+                                                   (bundled-form-id bundle-id (cl/log (-> bundled-table :definition first)))
                                                    (-> bundled-table :definition first)))
      (load-definition-fn db
                          (bundled-form-id bundle-id first-definition)
