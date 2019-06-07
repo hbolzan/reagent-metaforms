3c12fa7731e58c86c02a34f5247bf677d8b7582e
Henrique Bolzan <henrique@bolzan.com.br>
2019-06-07 07:44:28 -0300

    Another failed try

--- src/metaforms/modules/complex_forms/components/form.cljs
+++ src/metaforms/modules/complex_forms/components/form.cljs
@@ -1,7 +1,7 @@
 (ns metaforms.modules.complex-forms.components.form
   (:require [reagent.core :as r]
             [re-frame.core :as rf]
-            [ag-grid-react :refer [AgGridReact]]
+            [react-sticky-table :refer [StickyTable Row Cell]]
             [metaforms.modules.complex-forms.components.input :as input]
             [metaforms.components.cards :as cards]
             [metaforms.common.logic :as cl]
@@ -10,45 +10,31 @@
             [metaforms.modules.complex-forms.view-logic :as view-logic]))
 
 (defn complex-grid-header [fields-defs]
-  (let [key-prefix (random-uuid)]
-    [:thead
-     [:tr
-      (doall
-       (map (fn [field-def]
-              [:th {:key (str key-prefix "-" (:name field-def))} (:label field-def)])
-            fields-defs))]]))
+  [:> Row
+   (doall
+    (map (fn [field-def]
+           [:> Cell {:key (str "header-" (:name field-def))} (:label field-def)])
+         fields-defs))])
 
-(defn complex-grid-row [fields-defs row scroll-left]
-  (let [key-prefix (random-uuid)]
-    [:tr {:key (str key-prefix "-row")}
-     (doall
-      (map (fn [field-def]
-             [:td
-              {:key (str key-prefix "-" (:name field-def)) :data-scroll-left scroll-left}
-              (cl/log (get row (-> field-def :name keyword)))])
-           fields-defs))]))
+(defn complex-grid-row [fields-defs row]
+  [:> Row {:key (str "row-")}
+   (doall
+    (map (fn [field-def]
+           [:> Cell
+            {:key (str "field-" (:name field-def))}
+            (get row (-> field-def :name keyword))])
+         fields-defs))])
 
-(defn complex-grid-body [fields-defs data scroll-left]
-  [:tbody
-   (doall (map (fn [row] (complex-grid-row fields-defs row scroll-left)) data))])
+(defn complex-grid-body [fields-defs data]
+  (doall (map (fn [row] (complex-grid-row fields-defs row)) data)))
 
-(defn complex-grid [fields-defs data]
-  (let [scroll-info (atom {})
-        table-width (atom "")]
-    (fn [fields-defs data]
-      [:div.pane.pane--table2
-       {:onScroll #(swap! scroll-info assoc :left (-> % .-target .-scrollLeft))}
-       [:table.table {:width (cl/log @table-width)
-                      :data-scroll-left (-> @scroll-info :left)
-                      :ref (fn [el] (when (not (nil? el))
-                                      (reset! table-width
-                                              (str (+
-                                                    (cl/log (-> @scroll-info :left))
-                                                    (js/parseInt (.-width (js/window.getComputedStyle el))))
-                                                   "px"))))}
-        (complex-grid-header fields-defs)
-        (when (not-empty data)
-          (complex-grid-body fields-defs data (:left @scroll-info)))]])))
+(defn complex-grid [table-id fields-defs data]
+  [:div
+   [:div {:style {:width "100%" :height "350px"}}
+    [:> StickyTable
+     (complex-grid-header fields-defs)
+     (when (not-empty data)
+       (doall (map (fn [row] (complex-grid-row fields-defs row)) data)))]]])
 
 (defn form-child [key parent-id child-id]
   (let [child-form   @(rf/subscribe [:form-by-id child-id])
@@ -63,8 +49,7 @@
      [:div {:style {:min-height "100%"}}
       [:div.row
        [:div.col-md-12
-        [complex-grid (-> child-form :definition :fields-defs) data]
-        ]]]]))
+        [complex-grid child-id (-> child-form :definition :fields-defs) data]]]]]))
 
 ;; form-data => {:records [hash-map] :current-record integer :editing-data hash-map :new-record? boolean}
 (defn form-field
@@ -81,11 +66,6 @@
            (form-field field (view-logic/width->col-md-class bootstrap-width) form-state fields-defs))
          (view-logic/row-fields row-def fields-defs)
          (:bootstrap-widths row-def)))])
-
-(defn row-style [record-index params]
-  (js/console.log params)
-  (when (= record-index (-> (js->clj params) (get "data") (get "id")))
-    (clj->js {:background-color "#b7e4ff"})))
 
 #_(defn form-child [key parent-id child-id]
   (let [child-state (atom {})]
