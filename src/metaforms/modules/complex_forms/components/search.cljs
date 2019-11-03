b3fbd459e3d2c1fe63d5c2f904ee1e66e2a1d5f2
Henrique Bolzan <henrique@bolzan.com.br>
2019-11-03 17:42:02 -0300

    AG Grid implementation

--- src/metaforms/modules/complex_forms/components/search.cljs
+++ src/metaforms/modules/complex_forms/components/search.cljs
@@ -1,5 +1,6 @@
 (ns metaforms.modules.complex-forms.components.search
-  (:require [metaforms.common.dictionary :refer [l]]
+  (:require [ag-grid-react :refer [AgGridReact]]
+            [metaforms.common.dictionary :refer [l]]
             [metaforms.common.helpers :as helpers]
             [metaforms.common.logic :as cl]
             [metaforms.modules.complex-forms.logic :as cf.logic]
@@ -31,7 +32,7 @@
 
 (defn search-header [on-search-button-click]
   [:div.form-group
-   [:label {:for "search-field"} "Search"] ;; TODO: use dictionary
+   [:label {:for "search-field"} (l :common/search)]
    [:div.input-group.mb-3
     [:input.form-control {:type      "text"
                           :id        "search-field"
@@ -40,139 +41,103 @@
      [:button.btn.btn-primary {:type "button" :on-click (partial on-search-button-click* on-search-button-click)}
       [:i.fa.fa-search]]]]])
 
-(defn grid-th [{:keys [label name width]}]
-  [:th {:key (str "th-" name) :style {:min-width (str (* width 8) "px")}} label])
+;; ***************************************************** ;;
+;; *** AG-GRID
+;; ***************************************************** ;;
 
-(defn grid-head [fields-defs]
-  [:thead {:id "search-thead-id"}
-   [:tr
-    (doall (map grid-th fields-defs))]])
+(defn field-def->ag-grid-def
+  [{:keys [label name width data-type] :as field-def}]
+  {:headerName label
+   :field      name
+   :width      (* 8 width)
+   :resizable  true
+   :sortable   true
+   :filter     true})
 
-(defn grid-td [row-index col-index value]
-  [:td {:key (str "td-" row-index "-" col-index)} value])
+(defn fields-defs->ag-grid-defs [fields-defs]
+  (map field-def->ag-grid-def fields-defs))
 
-(defn bounding-info [el thead container]
-  (let [container-rect (-> container .getBoundingClientRect)
-        el-rect        (-> el .getBoundingClientRect)
-        thead-rect     (-> thead .getBoundingClientRect)]
-    {:container-rect        container-rect
-     :container-rect-top    (-> container-rect .-top)
-     :container-rect-height (-> container-rect .-height)
-     :container-scroll-top  (.-scrollTop container)
-     :thead-rect-height     (-> thead-rect .-height)
-     :el-rect-top           (-> el-rect .-top)
-     :el-rect-height        (-> el-rect .-height)}))
+(defn on-cell-key-press [on-search-select-record e]
+  (let [event    (.-event e)
+        key-code (.-keyCode event)]
+    (when (= key-code 13) (on-search-select-record (.-rowIndex e)))))
 
-(defn row-visible? [{:keys [container-rect-top container-rect-height el-rect-top thead-rect-height]}]
-  (and (> el-rect-top (+ container-rect-top thead-rect-height))
-       (< el-rect-top (+ container-rect-top container-rect-height))))
+(defn all-nodes [api]
+  (.-allLeafChildren (.-rootNode (.-rowModel api))))
 
-(defn scroll-to-visible! [container {:keys [container-rect-top container-scroll-top el-rect-top]}]
-  (dom.helpers/set-scroll-top container (+ container-scroll-top el-rect-top -240)))
+(defn node-by-index [nodes index]
+  (first (filter #(= index (.-rowIndex %)) nodes)))
 
-(defn grid-tr-ref [selected? el]
-  (when (and el selected?)
-    (let [thead     (dom.helpers/element-by-id "search-thead-id")
-          container (dom.helpers/first-element-by-class-name "tableFixHead")
-          boundings (bounding-info el thead container)]
-      (when-not (row-visible? boundings)
-        (scroll-to-visible! container boundings)))))
+(defn select-node [api node]
+  (when (and api node)
+    (.setSelected node true true)
+    (.setFocusedCell api
+                     (.-rowIndex node)
+                     (first (.-allDisplayedColumns (.-columnController node))))))
 
-(defn grid-tr [fields-defs on-click on-double-click selected? row-index row-data]
-  [:tr (merge {:key             (str "tr-row-" row-index)
-               :on-click        on-click
-               :on-double-click on-double-click
-               :ref             #(grid-tr-ref selected? %)}
-              (when selected?
-                {:style {:background-color "blue"
-                         :color            "white"}}))
-   (doall
-    (map-indexed
-     (fn [i {name :name}] (grid-td row-index i (get row-data (keyword name))))
-     fields-defs))])
+(defn select-row-by-index [api row-index]
+  (when (and api row-index)
+    (select-node api (node-by-index (all-nodes api) row-index))))
 
-(defn set-row-index! [grid-state* row-index entrypoint-id]
-  (let [current-id (:entrypoint-id @grid-state*)
-        id         (or entrypoint-id current-id)]
-    (when (or (nil? entrypoint-id) (not= id current-id))
-      (swap! grid-state* assoc :selected-row-index row-index :entrypoint-id id))))
+(defn data-changed-handler [e]
+  (let [api        (.-api e)
+        first-node (first (all-nodes api))]
+    (select-node api first-node)))
 
-(defn set-row-index [grid-state data set-fn]
-  (if-let [current-index (:selected-row-index grid-state)]
-    (let [index (set-fn current-index)]
-      (if (or (< index 0) (> index (-> data count dec))) current-index index))
-    0))
+(defn row-selected-handler [on-search-focus-record e]
+  (let [row-index (.-rowIndex e)
+        selected (.-selected (.-node e))]
+    (when selected
+      (on-search-focus-record row-index))))
 
-(defn set-row-index!! [grid-state* on-row-selected set-fn]
-  (let [data      @(rf/subscribe [:current-form-records])
-        row-index (set-row-index @grid-state* data set-fn)]
-    (set-row-index! grid-state* row-index nil)
-    (when (fn? on-row-selected) (on-row-selected row-index (get data row-index)))))
+(defn ag-grid-render [form-id defs height width rows events]
+  (let [state* (atom {})]
+    (r/create-class
+     {:display-name
+      "ag-search-grid"
+      :reagent-render
+      (fn [form-id defs height width rows {:keys [on-search-focus-record
+                                                  on-search-select-record]}]
+        (let [row-index @(rf/subscribe [:form-by-id-record-index form-id])]
+          (select-row-by-index (:api @state*) row-index)
+          [:div.ag-theme-balham {:style {:height height :width width}}
+           [:> AgGridReact {:columnDefs          defs
+                            :rowData             rows
+                            :rowSelection        "single"
+                            :onGridReady         (fn [e] (reset! state* {:api (.-api e)}))
+                            :onCellDoubleClicked #(on-search-select-record (.-rowIndex %))
+                            :onCellKeyPress      #(on-cell-key-press on-search-select-record %)
+                            :onRowDataChanged    data-changed-handler
+                            :onRowSelected       #(row-selected-handler on-search-focus-record %)
+                            :onCellFocused       (fn [e] (js/console.log (.-api e) (.-rowIndex e) row-index)
+                                                   ;; (select-row-by-index (.-api e) (.-rowIndex e))
+                                                   )
 
-(defn grid-body [grid-state* form-id fields-defs on-row-selected on-row-double-click data]
-  (let [selected-index (:selected-row-index @grid-state*)
-        on-row-click   (fn [row-index row-data e]
-                         (when (fn? on-row-selected) (on-row-selected row-index row-data))
-                         (set-row-index! grid-state* row-index nil))]
-    [:tbody
-     (doall
-      (map-indexed
-       (fn [i row] (grid-tr fields-defs
-                            #(on-row-click i row %)
-                            #(on-row-double-click i row)
-                            (= i selected-index) i row))
-       data))]))
+                            }]]))})))
 
-(defn set-keybindings [grid-state* on-row-selected]
-  [:<>
-   [kb/kb-action "esc" (fn [] (rf/dispatch [:modal-close]))]
-   [kb/kb-action "up" (fn [] (set-row-index!! grid-state* on-row-selected dec))]
-   [kb/kb-action "down" (fn [] (set-row-index!! grid-state* on-row-selected inc))]])
-
-(defn grid-render
-  [grid-state* form-id fields-defs on-search-button-click on-row-double-click on-row-selected]
-  (let [data @(rf/subscribe [:current-form-records])]
+(defn ag-search-grid
+  [form-id fields-defs {:keys [on-search-button-click] :as events}]
+  (let [height    (str (- (-> js/window .-visualViewport .-height) 340) "px")
+        width     (str (- (-> js/window .-visualViewport .-width) 50) "px")
+        ag-defs   (fields-defs->ag-grid-defs fields-defs)
+        rows      @(rf/subscribe [:current-form-records])]
     [:div {:style {:min-height "100%"}}
-     (set-keybindings grid-state* on-row-selected)
      [:div.row
       [:div.col-md-12
        (search-header on-search-button-click)]]
-     [:div.tableFixHead {:style {:max-height (- (-> js/window .-visualViewport .-height) 340)}}
-      [:table.table.table-bordered.table-hover
-       (grid-head fields-defs)
-       (grid-body grid-state* form-id fields-defs on-row-selected on-row-double-click data)]]]))
-
-(defn search-grid
-  [form-id fields-defs row-index entrypoint-id on-search-button-click on-row-double-click on-row-selected]
-  (let [grid-state* (r/atom {})]
-    (r/create-class
-     {:display-name   "search-grid"
-      :reagent-render (fn [form-id
-                           fields-defs
-                           row-index
-                           entrypoint-id
-                           on-search-button-click
-                           on-row-double-click
-                           on-row-selected]
-                        (set-row-index! grid-state* row-index entrypoint-id)
-                        (grid-render grid-state*
-                                     form-id
-                                     fields-defs
-                                     on-search-button-click
-                                     on-row-double-click
-                                     on-row-selected))})))
+     [:div.row
+      [:div.col-md-12
+       [ag-grid-render form-id ag-defs height width rows events]]]]))
 
 (defn start
   [db form-id]
   {:dispatch
    [:show-modal-window
     (l :common/search)
-    [search-grid
+    [ag-search-grid
      form-id
      (cf.logic/fields-defs db)
-     (cf.logic/form-by-id-current-record-index db form-id)
-     (random-uuid)
-     (fn [search-value] (rf/dispatch [:search-button-click form-id search-value]))
-     (fn [row-index selected-object] (rf/dispatch [:form-search-select-record form-id row-index]))
-     (fn [row-index row-data] (rf/dispatch [:search-grid-select-row form-id row-index]))]
+     {:on-search-button-click  #(rf/dispatch [:search-button-click form-id %])
+      :on-search-focus-record  #(rf/dispatch [:search-grid-select-row form-id %])
+      :on-search-select-record #(rf/dispatch [:form-search-select-record form-id %])}]
     #(on-confirm-button-click form-id)]})
